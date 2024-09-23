95024380c5408debea6018527c6deb3f70380c85
rajput7799 <royalrajputchetan2003@gmail.com>
2024-09-23 13:57:00 +0530

    left uncommitted

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) =>{
+    res.send("hello")
+})
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("app is listening on port 3000");
+    
+})
